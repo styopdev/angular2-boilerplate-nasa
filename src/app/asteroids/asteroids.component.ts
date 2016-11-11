53d31a664d1e4a70de78fbb245aced3148e8ce6c
Stepan Vardanyan <styopdev@hotmail.com>
2016-11-11 16:23:59 +0400

    add week range validation.

--- src/app/asteroids/asteroids.component.ts
+++ src/app/asteroids/asteroids.component.ts
@@ -11,12 +11,30 @@ export class AsteroidsComponent {
 	startDate: string = '';
 	endDate: string = '';
   asteroids: any;
-  constructor(private nasa: NasaService) {}
+  invalidRange: boolean;
 
-	go() {
+  constructor(private nasa: NasaService) {
+  }
+  
+  validateRange () {
+    this.invalidRange = false;
+    var oneDay = 24 * 60 * 60 * 1000;
+    var firstDate = new Date(this.startDate);
+    var secondDate = new Date(this.endDate);
+    var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+    if (diffDays > 7) {
+      this.invalidRange = true;
+    }
+    return this.invalidRange;
+  }
+	
+  go() {
+    if (this.validateRange()) {
+      return;
+    }
 		this.nasa.getAsteroids(this.startDate, this.endDate)
 		 	.subscribe(asteroids => {
-         this.asteroids = new Array();
+        this.asteroids = new Array();
         for (let entry in asteroids.near_earth_objects) {
           this.asteroids = this.asteroids.concat(asteroids.near_earth_objects[entry]);
         }
