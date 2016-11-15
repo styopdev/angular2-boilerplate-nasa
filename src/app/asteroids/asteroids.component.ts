92b9107c01d49b0240576ecdcf0bbc46ae66d43d
Stepan Vardanyan <styopdev@hotmail.com>
2016-11-15 17:43:56 +0400

    Ui fixes, refactor

--- src/app/asteroids/asteroids.component.ts
+++ src/app/asteroids/asteroids.component.ts
@@ -38,7 +38,6 @@ export class AsteroidsComponent {
     let asteroidsFormatted = new Array();
     this.nasa.getAsteroids(this.startDate, this.endDate)
        .subscribe(asteroids => {
-         console.log(asteroids);
         for (let date in asteroids.near_earth_objects) {
           for (let key in asteroids.near_earth_objects[date]) {
             let ast = asteroids.near_earth_objects[date][key];
@@ -47,7 +46,7 @@ export class AsteroidsComponent {
               name: ast.name,
               distance: ast.close_approach_data[0].miss_distance.kilometers,
               absolute_magnitude_h: ast.absolute_magnitude_h,
-              is_potentially_hazardous: ast.is_potentially_hazardous_asteroid,
+              is_potentially_hazardous: (ast.is_potentially_hazardous_asteroid ? 'Yes' : 'No'),
               diameter: ast.estimated_diameter.meters.estimated_diameter_min + ' - ' + ast.estimated_diameter.meters.estimated_diameter_max
             }
             asteroidsFormatted.push(asteroid);
